1ea8077f7232316cd456c368278e8c3a0dfe80a2
SashaRedka <dev.redka@gmail.com>
2021-03-16 15:32:11 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHJ44K3rz8bZS9QhjNvFkw9jnOdWpXL94",
+    authDomain: "slack-74f50.firebaseapp.com",
+    projectId: "slack-74f50",
+    storageBucket: "slack-74f50.appspot.com",
+    messagingSenderId: "830015296637",
+    appId: "1:830015296637:web:87498bc7953d8686e1cd14"
+  };
